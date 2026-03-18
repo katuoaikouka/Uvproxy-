c75a71db938475c10767bd62693944afe8ca9793
katuoaikouka <yosinaka081@gmail.com>
2026-03-18 14:59:58 +0900

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+importScripts("/uv/uv.bundle.js");
+importScripts("/uv/uv.config.js");
+importScripts("/uv/uv.sw.js");
+
+const uv = new UVServiceWorker();
+
+self.addEventListener("fetch", (event) => {
+  // UVのプレフィックスで始まるリクエストをインターセプト
+  if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
+    event.respondWith(uv.fetch(event));
+  }
+});
