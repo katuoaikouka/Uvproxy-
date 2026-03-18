73bef555ea9756be87f0c8c26b2e785d23fbed17
katuoaikouka <yosinaka081@gmail.com>
2026-03-18 20:18:15 +0900

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: window.location.origin + '/bare/', // 絶対パスにすることでパースミスを防ぐ
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
